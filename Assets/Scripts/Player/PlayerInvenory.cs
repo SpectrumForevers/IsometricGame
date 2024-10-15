e349047c56f3777dcf6ef148f58c8c914d9d6cd9
Spectrum <113600641+SpectrumForevers@users.noreply.github.com>
2024-10-15 21:51:33 +0300

    add enemy ||| change weapon

--- Assets/Scripts/Player/PlayerInvenory.cs
+++ Assets/Scripts/Player/PlayerInvenory.cs
@@ -6,6 +6,43 @@ public class PlayerInvenory : MonoBehaviour
 {
     [SerializeField] GameObject weaponActive;
     [SerializeField] List<GameObject> weapons = new List<GameObject>();
+    private int currentWeaponIndex = 0; // Индекс текущего активного оружия
 
+    void Start()
+    {
+        // Убеждаемся, что первое оружие активно при старте
+        SelectWeapon(currentWeaponIndex);
+    }
 
+    void Update()
+    {
+        // Получаем значение скролла колеса мыши
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scrollInput > 0f)
+        {
+            // Переключение на следующее оружие
+            currentWeaponIndex = (currentWeaponIndex + 1) % weapons.Count;
+            SelectWeapon(currentWeaponIndex);
+        }
+        else if (scrollInput < 0f)
+        {
+            // Переключение на предыдущее оружие
+            currentWeaponIndex--;
+            if (currentWeaponIndex < 0)
+            {
+                currentWeaponIndex = weapons.Count - 1; // Циклическое переключение
+            }
+            SelectWeapon(currentWeaponIndex);
+        }
+    }
+
+    void SelectWeapon(int index)
+    {
+        // Проходим по всем объектам в листе и активируем только выбранное оружие
+        for (int i = 0; i < weapons.Count; i++)
+        {
+            weapons[i].SetActive(i == index);
+        }
+    }
 }
