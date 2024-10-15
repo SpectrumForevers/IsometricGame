a2c1e3804443de1a2b150f4752198dc5f7d31981
Spectrum <113600641+SpectrumForevers@users.noreply.github.com>
2024-10-15 22:13:14 +0300

    removeEnemy

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] PlayerInvenory playerInvenory;
+    [SerializeField] PlayerController playerController;
+    [SerializeField] PlayerMove playerMove;
+
+    [SerializeField] float playerHP;
+    [SerializeField] float playerArmor;
+
+
+
+}
